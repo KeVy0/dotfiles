fc00dabb5d98558b028e183ae7883b5bab074e4c
KeVy0 <kevy0sec@gmail.com>
2017-02-04 00:18:34 +0100

    Add files via upload

---
+++ spotify.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+import dbus
+
+bus = dbus.SessionBus()
+spotify = bus.get_object("org.mpris.MediaPlayer2.spotify", "/org/mpris/MediaPlayer2")
+spotify_iface = dbus.Interface(spotify, 'org.freedesktop.DBus.Properties')
+props = spotify_iface.Get('org.mpris.MediaPlayer2.Player', 'Metadata')
+print(str(props['xesam:artist'][0]) + " - " + str(props['xesam:title']))
